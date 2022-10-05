68c7846ea49fdf945ac127006aee38305838f430
Sirin Aktas <sirinaktas@live.Fr>
2022-10-05 17:37:27 +0200

    read me

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # p7_groupomania
+
+Social media for the crew [Groupomania]
+
+## Step for start the server
+
+First of all do a : cd back \
+then : npm run start \
+With this you will connect mongodb to the localhost \
+
+Then : cd front \
+and : npm run start \
+With this you will lunch react app server on localhost
--- back/utils/errors.utils.js
+++ back/utils/errors.utils.js
@@ -2,13 +2,13 @@ module.exports.signUpErrors = (error) => {
     let errors = { pseudo: "", email: "", password: "" };
 
     if (error.message.includes('pseudo'))
-        errors.pseudo = "Pseudo incorrect ou déjà pris";
+        errors.pseudo = "Pseudo incorrect (min 3 caractères) et/ou déjà pris";
 
     if (error.message.includes('email'))
-        errors.email = "Email incorrect ou déjà pris";
+        errors.email = "Email incorrect et/ou déjà pris";
 
     if (error.message.includes('password'))
-        errors.password = "Mot de passe incorrect ou trop court (6 caractères min)";
+        errors.password = "Mot de passe trop court (6 caractères min)";
 
     return errors;
 };
@@ -20,7 +20,7 @@ module.exports.signInErrors = (error) => {
         errors.email = "Email incorrect ou inconnu";
 
     if (error.message.includes('password'))
-        errors.password = "Mot de passe incorrect ou trop court";
+        errors.password = "Mot de passe incorrect";
 
     return errors;
 };
